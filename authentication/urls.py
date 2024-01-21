3e75b7b10363bc06307ff630b00c5028423c71d9
Talantone <talgatsitd@gmail.com>
2024-01-21 11:51:59 +0100

    updated to REST standard

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,5 +5,5 @@ from authentication.views import RegisterView, ForgotPasswordView, ResetPassword
 urlpatterns = [
     path("register/", RegisterView.as_view(), name='register'),
     path("forgot-password/", ForgotPasswordView.as_view(), name='forgot-password'),
-    path("reset-password/<str:access>", ResetPasswordView.as_view(), name='reset-password'),
+    path("reset-password/<str:access>/", ResetPasswordView.as_view(), name='reset-password'),
 ]
